7566929f7d557d10e1813310bd8d6f04ea947dfd
lencx <cxin1314@gmail.com>
2021-09-17 01:20:53 +0800

    fix: giscus pathname not work

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,21 @@
-import DefaultTheme from 'vitepress/theme'
-import Layout from './Layout.vue'
-import './main.scss'
+import DefaultTheme from "vitepress/theme";
+import { watch } from "vue";
+import Layout from "./Layout.vue";
+import "./main.scss";
 
 export default {
   ...DefaultTheme,
   Layout,
-}
+  enhanceApp({ router }) {
+    watch(
+      () => router.route.path,
+      (path) => {
+        const _path = localStorage.getItem("URL_PATH", path);
+        if (_path !== path) {
+          localStorage.setItem("URL_PATH", path);
+          window.location.reload();
+        }
+      }
+    );
+  },
+};
